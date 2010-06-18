50a1b21b8598065f4ebdbef11cd241d25a2a0e4c
legendar <legendar@gmail.com>
2010-06-19 00:50:39 +0300

    img.tpl.php now support svg files

--- engine/site/templates/img.tpl.php
+++ engine/site/templates/img.tpl.php
@@ -1,14 +1,20 @@
 <?
-
 if(empty($img)) error404();
 
 clearBuffer();
+
+if(substr($ext, -1) == '/') {
+    $ext = substr($ext, 0, -1);
+}
 
 switch($ext) {
     case "png":
     case "gif":
     case "ico":
         $type = "image/{$ext}";
+        break;
+    case "svg":
+        $type = 'image/svg+xml';
         break;
     case "jpeg":
     case "jpg":
